441cda9a514cf56bcf7b71dba798985a8cb3b160
amaydeveloper <amaydeveloper@MacBook-Pro-tilhrende-amaydeveloper.local>
2022-05-18 16:56:28 +0200

    last

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,34 +1,9 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import StyledTag from '../components/StyledTag';
-
 const Setting = () => {
-  let navigate = useNavigate();
   return (
     <>
       <StyledTag bold />
-      <div
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginTop: '5rem',
-          display: 'flex',
-          width: '30rem',
-          height: '100%',
-        }}
-      >
-        <div>
-          <h1
-            style={{ cursor: 'pointer' }}
-            onClick={() =>
-              navigate('/companysettng/projectsettings/spicialproject')
-            }
-          >
-            Go to the last page
-          </h1>
-        </div>
-        <div>(only for testing purpose)</div>
-      </div>
     </>
   );
 };
